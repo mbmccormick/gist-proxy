5057374b0a5cbf4b36e11f84984084661eeac720
Matt McCormick <mbmccormick@gmail.com>
2011-11-04 12:10:22 -0300

    Fixing PHP syntax error, moving cache data to separate folder.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     $gist = $_GET[id];
     
     // check if we have need to update our local copy of the gist
-    if (file_exists($gist . ".js") == false ||
-        (date() - filemtime($gist . ".js")) > 1209600) // if local copy is greater than 14 days old
+    if (file_exists("gists/" . $gist . ".js") == false ||
+        (time() - filemtime("gists/" . $gist . ".js")) > 1209600) // if local copy is greater than 14 days old
     {
         // download javascript file from github
         $ch = curl_init("https://gist.github.com/" . $gist . ".js");
-        $fp = fopen($gist . ".js", "wb");
+        $fp = fopen("gists/" . $gist . ".js", "wb");
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_exec($ch);
@@ -18,7 +18,7 @@
     }
     
     // open local copy of gist
-    $fp = fopen($gist . ".js", "r");
+    $fp = fopen("gists/" . $gist . ".js", "r");
     
     // parse javascript and render
     if ($fp)
